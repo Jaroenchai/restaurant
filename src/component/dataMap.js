7d3f3ad81227faa493f72f8ebffdabe7643cf0d8
Jaroenchai <kkllooii009@gmail.com>
2022-06-03 21:45:22 +0700

    update 3/6/2020

---
+++ src/component/dataMap.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import json from "../data.json";
+import "./dataMap.css";
+
+function Data() {
+  const items = (
+    json.map((item, index) => {
+      return (
+        <div className="content" key={index}>
+          <div className="name-res">
+            <img className="img-bk" src={item.profile_image_url} />
+            <div className="text">
+              {item.name}
+              <div>
+                {item.operation_time[0].time_open}{" "}
+                {item.operation_time[0].time_close}
+              </div>
+            </div>
+            <p className="rating">★{item.rating}</p>
+          </div>
+          <div className="img-detail">
+            <img className="img-food border-left" src={item.images[0]} />
+            <img className="img-food" src={item.images[1]} />
+            <img className="img-food border-right" src={item.images[2]} />
+          </div>
+        </div>
+      );
+    })
+  );
+
+  return <div className="grid">{items}</div>;
+}
+
+export default Data;
